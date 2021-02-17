be4386c9e9ad21cf6c06dd3797e9a13379e17732
alex2mendosa <66001736+alex2mendosa@users.noreply.github.com>
2021-02-17 17:35:27 +0200

    Update 3_knn_1.R

--- 3_knn_1.R
+++ 3_knn_1.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(tibble)
 library(ggplot2)
 library(tidyr)
+library(class)
+
 n<-1000
 sample_1<-tibble( x1=rnorm(n,10,2),
                   x2=runif(n, min = 100, max = 200) , 
@@ -13,15 +15,16 @@ sample_1<-tibble( x1=rnorm(n,10,2),
 glimpse(sample_1)
 
 #######################  PART_1
-# knn is example of supervised , classification technique
+# knn is example of supervised classification technique
 
 
 #  To initiate knn the follwong action shoul be taken:
 #  Data inspection and normalisation
-#  Splitin data into training and testing datasets
+#  Splitting data into training and testing datasets
 #  Fitting the model to training data
 #  Evaluate perfomance on test data
 #  Model tuning , specifically, for knn, determining optimal values of k
+   # optimal normalisation procedure
 
 # knn is extremely powerfull when applied
 # to homogenous data, speficically,
@@ -30,19 +33,21 @@ glimpse(sample_1)
 # General routone for knn is the following:
 ## ?
 
-#  Now how to measure similary of instances or records
-#  Traditionally euclidian distance us applied
+#  Now, how to measure similary of instances or records
+#  Traditionally Euclidian distance us applied
 
-# assume 2 dimentional space with 2 poins
+# assume 2 dimentional space with 2 points
 
 sample_2<-data.frame(  feature_BMI=c( runif(1,18.5,24.9),
                                       runif(1, 25.0,29.9)),
                        class=c( "Healthy weight","Overweight" ),
                        row.names=c("inst_1  ","inst_2  ")   )
 
+View(sample_2)
+
 
 ggplot(data=sample_2,aes(y= feature_BMI ,x=class))+
-  geom_point(col="red",cex=3)+
+  geom_point(col="red",cex=4)+
   geom_label(aes(label=round(feature_BMI,2)  ),nudge_y = 0.5 )
 
 
@@ -60,22 +65,17 @@ ggplot(data=sample_2,aes(y= feature_BMI ,x=class))+
   geom_segment(x="Healthy weight",y=new_point,xend = "Healthy weight", yend = sample_2$feature_BMI[1],
                color="green")+
   geom_segment(x="Overweight",y=new_point,xend = "Overweight", yend = sample_2$feature_BMI[2],
-               color="green")
+               color="green")+
+  geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2 )
 
 
-
-
-tibble::glimpse(sample_2)
-sample_2
-
-# lest estimate Euclidian Distance between 2 poins, we have only 1 instance
-#per feature , threfore we would check knn where k in one 
+# Lets estimate Euclidian Distance between 2 poins, we have only 1 instance
+# per feature , threfore we would check knn where k in one 
 
 dist_1<- sqrt( (new_point-sample_2$feature_BMI[1])^2 )
 dist_2<- sqrt( (new_point-sample_2$feature_BMI[2])^2 )
 
 sample_2["Eu_Dist"]<-c(dist_1 ,dist_2 )
-
 
 sample_2<-sample_2 %>% mutate(Eu_Dist=sqrt( (new_point-feature_BMI)^2 ))
 
@@ -85,49 +85,82 @@ sample_2<-sample_2 %>% dplyr::bind_rows( tibble(feature_BMI=new_point,class=NA,E
 sample_2$class[3]<-"Healthy weight"
 sample_2$class[3]<-sample_2$class[ which.min(sample_2$Eu_Dist) ]
 
+class::knn( train=sample_2[c(1,2),1],
+            test=new_point,
+            cl=sample_2[c(1,2),2],
+            k=1, prob=TRUE )
 
-class::knn( sample_2[c(1,2),1],new_point,sample_2[c(1,2),2],k=1, prob=TRUE )
 
-#######################  PART_2
+ggplot(data=sample_2,aes(y= feature_BMI ,x=class))+
+  geom_point(col="red",cex=4)+
+  geom_label(aes(label=round(feature_BMI,2)  ),nudge_y = 0.1,nudge_x=0.2 )+
+  geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) +
+  geom_segment(x="Healthy weight",y=new_point,xend = "Healthy weight", yend = sample_2$feature_BMI[1],
+               color="green")+
+  geom_segment(x="Overweight",y=new_point,xend = "Overweight", yend = sample_2$feature_BMI[2],
+               color="green")+
+  geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2 )+
+  geom_label(aes(label=paste( "sqrt","(","(",round(new_point,2),"-",
+                              round(feature_BMI[1],2) ,")","^2",")"),
+                                  x="Healthy weight",y=new_point  ),nudge_y = 2,nudge_x=0 )
+
+
+
+                          #######################  PART_2
 
 sample_3<-data.frame(  feature_BMI=c( runif(3,18.5,24.9),
                                       runif(3, 25.0,29.9)),
                        class=c( rep("Healthy weight",3),rep("Overweight",3) ) )
 
+ggplot(data=sample_3,aes(y= feature_BMI ,x=class))+
+  geom_point(col="red",cex=6)+
+  geom_label(aes(label=round(feature_BMI,2)  ),nudge_y = 0.1,nudge_x=0.2 )+
+  geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) +
+  geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2,color="blue" )
+  
+
+sample_3<-sample_3 %>% mutate(Eu_Dist=sqrt( (new_point-feature_BMI)^2 ))
+
 
 ggplot(data=sample_3,aes(y= feature_BMI ,x=class))+
   geom_point(col="red",cex=6)+
   geom_label(aes(label=round(feature_BMI,2)  ),nudge_y = 0.1,nudge_x=0.2 )+
-  geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) 
-
-sample_3<-sample_3 %>% mutate(Eu_Dist=sqrt( (new_point-feature_BMI)^2 ))
+  geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) +
+  geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2,color="blue" )+
+  geom_label(aes(label=paste( "sqrt","(","(",round(new_point,2),"-",round(feature_BMI[1],2) ,")","^2","+","\n",
+                                         "(",round(new_point,2),"-",round(feature_BMI[2],2) ,")","^2","+","\n",
+                                         "(",round(new_point,2),"-",round(feature_BMI[3],2) ,")","^2",")"),
+                 x="Healthy weight",y=new_point  ),nudge_y = 2,nudge_x=0 )
+  
 
 # Assume that k equals 3 , threrefore we need to choode free nearest neighbours
 
 sample_3 <-sample_3 %>% arrange(Eu_Dist) %>% slice(1:3)
 
+table(sample_3$class)
 prop.table( table(sample_3$class) )
 
-class::knn( sample_3[,1],new_point,sample_3[,2],k=3, prob=TRUE )
+class::knn( train=sample_3[,1],test=new_point,cl=sample_3[,2],k=3, prob=TRUE )
 
 
 
 
-##### PART 3
+                       ############ PART 3
 
 sample_4<-iris
 View(sample_4)
 
 
-# in this example there are 4 features which describe plant
-# out target class is species column 
+# in this example there are 4 features which 
+# are characeristics of 3 different plants, 
+# out target is to find class[Species] of 
+# plant fiven its 4 featues
 
-# lets draw single row we would like to preddict
 
-loc<-sample(1:nrow(sample_4),1 )
-test_row_1<-sample_4[ loc   ,  ]
+# lets draw single row we would like to predict
 
-train_row<-sample_4[ -loc   ,  ]
+loc<-sample(1:nrow(sample_4),3 ) # we randomly generate 3 indices
+test_row_1<-sample_4[ loc[1]   , -5  ]
 
 # what classes are available 
 sample_4$Species %>% unique()
@@ -136,11 +169,11 @@ sample_4$Species %>% unique()
 # here are 4 features which all should be part
 # of equation 
 
-# lest fraw 4 graphs, 1 graph per feature where each graph contains
-# 1 point from test set and 1 form training set
+# First off all wee ned to draw 4 graphs, 
+# 1 graph per feature where each graph contains
+# 1 point from test set and 2 form training set
 
-loc<-sample(1:nrow(train_row),2 )
-train_row_2<-train_row[loc,]
+train_row_2<-sample_4[loc[c(2,3)],  ]
 
 # 
 train_row_2
@@ -148,26 +181,30 @@ test_row_1
 
 # here we have 2 instances from taining set and 
 # we need to decide which is closer in terms of 
-#euclidian distance to test_row_1
+# euclidian distance to test_row_1
 
-# we need to bind data into tibble to visualise data
-# this transformation i srequired 
+# we need to bind data into tibble to produce meaningfull plot
+# this transformation is srequired 
 # to apply ggplot with facets
 gg_train<-train_row_2 %>% pivot_longer(Sepal.Length:Petal.Width ,
-                                       names_to="Feature")
+                                       names_to="Feature")%>%
+  rename("Class"="Species")  
+
 gg_test<-test_row_1 %>% pivot_longer(Sepal.Length:Petal.Width ,
                                      names_to="Feature") %>%
-  mutate(C="Unknown")  
+         mutate(Class="Unknown") %>% select(Class,Feature,value)
 
 gg_in<-bind_rows(gg_train,gg_test)
-ggplot(data=gg_in,aes(y=Species,x=value))+ 
-  geom_point(aes(col= Species),cex=3)+
-  geom_label(aes(label=round(value,2),color=Species )   ) + 
-  facet_grid(~Feature, scales="free_x") 
+
+ggplot(data=gg_in,aes(y=Class,x=value))+ 
+  geom_point(aes(col= Class),cex=5)+
+  geom_text(aes(label=round(value,2),color=Class ),cex=4,nudge_y = 0.2 ) + 
+  facet_grid(~Feature,  space="free") +
+  theme_bw()
 
 
-
-train_row_2[,-5]-test_row_1[ c(1,1),-5 ]
+train_row_2[,-5]-test_row_1[ c(1,1),-5 ] 
+# this is how to extract values element wise
 
 (train_row_2[,-5]-test_row_1[ c(1,1),-5 ])^2
 
@@ -177,13 +214,14 @@ train_row_2<- train_row_2 %>%
   mutate(Eu_Dist=Eu_Dist) %>%
   arrange(Eu_Dist)
 
-knn( train=train_row_2[,-5],
+class::knn( train=train_row_2[,c(-5,-6)],
      test=test_row_1[ ,-5 ],
      cl=train_row_2[,5],
      prob=TRUE)
 
 
-### Lets apply knn to each onbservation 
+
+##### Lets apply knn to each onbservation 
 
 
 loc<-sample(1:nrow(sample_4),20 )
@@ -199,62 +237,62 @@ sum(m1_knn==test_set[ ,5 ])/ length(test_set[ ,5 ])
 
 ##  now lest write our own fucntion to come up with solution 
 
-  test<-sample_4[ loc   ,  ]
-  train<-sample_4[ -loc   ,  ]
-  out_2<- test[,5]
-  
-    n_test<-nrow(test)
-    n_train<-nrow(train)
-    out<-rep("",n_test)
-    
-    for ( i in 1:n_test) {
-    Eu_Dist=rowSums( (train[,-5]-test[i,-5][c(rep(1,n_train)), ] )^2 ) %>% sqrt()
-    train_1<-train %>% mutate(Eu_Dist=Eu_Dist) %>% arrange(Eu_Dist) %>%
-                                             slice(1:3)
-    class_1<-names( which.max(table(train_1$Species)))
-    out[i]<-class_1
-                         } 
+test<-sample_4[ loc   ,  ]
+train<-sample_4[ -loc   ,  ]
+out_2<- test[,5]
 
-    sum(out==out_2)/ n_test
-    
-    # identical 
+n_test<-nrow(test)
+n_train<-nrow(train)
+out<-rep("",n_test)
+
+for ( i in 1:n_test) {
+  Eu_Dist=rowSums( (train[,-5]-test[i,-5][c(rep(1,n_train)), ] )^2 ) %>% sqrt()
+  train_1<-train %>% mutate(Eu_Dist=Eu_Dist) %>% arrange(Eu_Dist) %>%
+    slice(1:3)
+  class_1<-names( which.max(table(train_1$Species)))
+  out[i]<-class_1
+} 
+
+sum(out==out_2)/ n_test
+
+# identical 
 
 
 # now lets multiply each column by random number
-    loc<-sample(1:nrow(sample_4),20 )
- sample_5<-iris
- sample_5[,c(1)]<-apply(sample_5[,c(1)],MARGIN=1:2, 
-                   function(x) return( x*runif(1,10,10000))  )
- sample_5[,c(1)]/iris[,c(1)] 
-   
-    test<-sample_5[ loc   ,  ]
-    train<-sample_5[ -loc   ,  ]
-    out_2<- test[,5]
-    
-    n_test<-nrow(test)
-    n_train<-nrow(train)
-    out<-rep("",n_test)
-    
-    m1_knn<-knn( train=train[,-5],
-                 test=test[ ,-5 ],
-                 cl=train[,5], k=3,
-                 prob=TRUE)
-    
-    sum(m1_knn==test[ ,5 ])/ length(test[ ,5 ])
+loc<-sample(1:nrow(sample_4),20 )
+sample_5<-iris
+sample_5[,c(1)]<-apply(sample_5[,c(1)],MARGIN=1:2, 
+                       function(x) return( x*runif(1,10,10000))  )
+sample_5[,c(1)]/iris[,c(1)] 
+
+test<-sample_5[ loc   ,  ]
+train<-sample_5[ -loc   ,  ]
+out_2<- test[,5]
+
+n_test<-nrow(test)
+n_train<-nrow(train)
+out<-rep("",n_test)
+
+m1_knn<-knn( train=train[,-5],
+             test=test[ ,-5 ],
+             cl=train[,5], k=3,
+             prob=TRUE)
+
+sum(m1_knn==test[ ,5 ])/ length(test[ ,5 ])
 
 
-    normalize<-function(x){
-      return((x-min(x))/(max(x)-min(x)))
-    }      
-    
-    train[,-5]<-apply(train[,-5], MARGIN=2 ,normalize)
-    
-    m1_knn<-knn( train=train[,-5],
-                 test=test[ ,-5 ],
-                 cl=train[,5], k=3,
-                 prob=TRUE)
-    
-    sum(m1_knn==test[ ,5 ])/ length(test[ ,5 ])
-    
-    
- 
+normalize<-function(x){
+  return((x-min(x))/(max(x)-min(x)))
+}      
+
+train[,-5]<-apply(train[,-5], MARGIN=2 ,normalize)
+
+m1_knn<-knn( train=train[,-5],
+             test=test[ ,-5 ],
+             cl=train[,5], k=3,
+             prob=TRUE)
+
+sum(m1_knn==test[ ,5 ])/ length(test[ ,5 ])
+
+
+
