c37325d368c631a2896e8b888e9ecbbdbd438859
alex2mendosa <66001736+alex2mendosa@users.noreply.github.com>
2021-02-18 16:19:57 +0200

    Update 3_knn_1.R

--- 3_knn_1.R
+++ 3_knn_1.R
@@ -1,47 +1,41 @@
 # we would require 5 libraries to explore and 
 # learn mechanics of k nearest neighbours
+
 library(dplyr) # data wrangling
 library(tidyr) # data wrangling
 library(tibble) # data wrangling
 library(ggplot2) # visuals
 library(class)  # model building
 
-# n<-1000
-# sample_1<-tibble( x1=rnorm(n,10,2),
-#                   x2=runif(n, min = 100, max = 200) , 
-#                   x3=rexp(n,0.1) , 
-#                   x4=sample(0:1,n,replace = TRUE) , 
-#                   x5=rchisq(n, 5, ncp = 50) ,
-#                   x6=log( rnorm(n,10,2) ) )
-# 
-# glimpse(sample_1)
 
 #######################  PART_1
 # knn is example of supervised classification technique
 # supervised means than we know all classes which corresponds
-# to specific records or instances of feature or variables 
+# to specific set of feature or variables grouped by rows or instances
 
 # classification means that we would model output 
-# as variable of  character or factor type
+# as variable ofcharacter or factor type
 
 #  To initiate knn the follwong action shoul be taken:
 #  Data inspection 
 #  Splitting data into training and testing datasets
 #  Fitting the model to training data
 #  Evaluate perfomance on test data
-#  Model tuning , specifically, for knn, determining optimal values of k
+#  Model tuning , specifically, for knn, to determine optimal values of k
 
 
 # knn is extremely powerfull when applied
 # to homogenous data, speficically,
-# it is suited very well to numerical features
+# it is suited very well to numerical features whose 
+# data range is similar
 
 # Goal of Knn is to 
-# Estimate similarity between records with degined classes and 
-# abd records without defined class, using arithmetic operations
+# Estimate similarity between records with defined classes and 
+# and  records without defined class
 
-#  Now, how to measure similary of instances or records
-#  Traditionally Euclidian distance is applied
+#  Now, how to measure similary of instances or records?
+#  We need to employ distacne formula,
+# Euclidian distance is a common approach
 
 # assume 2 dimentional space with 2 points
 
@@ -53,16 +47,17 @@ sample_1<-data.frame(  feature_BMI=c( runif(1,18.5,24.9),
 # we can use View to explicitly check all records
 View(sample_1)
 
+#Greate job 
 # Now lets visualise situation we are dealing with 
 ggplot2::ggplot(data=sample_1,aes(y= feature_BMI ,x=class))+
   geom_point(col="red",cex=5)+
   geom_label(aes(label=round(feature_BMI,2)  ),nudge_y = 0.5 )
 
-# we have 2 values for signle feature,
+# we have 2 values for single feature,
 # each value corresponds to specific class
-# Now assume , we need to decide , to which class 
+# Now assume , we need to decide , to what class 
 # we should assign new point , should it belong to 
-# Forst or second classm Healhy or overweight
+# Forst or second class: Healhy or overweight
 
 set.seed(123)
 new_point<-runif(1,18.5,29.9) # generate random point
@@ -78,7 +73,7 @@ ggplot2::ggplot(data=sample_1,aes(y= feature_BMI ,x=class))+
                color="green")+
   geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2 )
 
-# ble point can potentially belong to class 1 or 2
+# blue point can potentially belong to class 1 or 2
 # we need to measure distance between red and blue point, 
 # green and blue point
 
@@ -89,6 +84,7 @@ dist_1<- sqrt( (new_point-sample_1$feature_BMI[1])^2 )
 dist_2<- sqrt( (new_point-sample_1$feature_BMI[2])^2 )
 
 #lets add new column to sample_1
+# here is common way to add column to data frame
 sample_1["Eu_Dist"]<-c(dist_1 ,dist_2 )
 
 #alternativeluy, we can use mutate fucntion
@@ -104,10 +100,12 @@ row.names(sample_1)[3]<-3
 
 
 sample_1$class[3]<-"Healthy weight" # assign poing manually
-sample_1$class[3]<-sample_1$class[ which.min(sample_1$Eu_Dist) ] # use which.min, ignores NA
+sample_1$class[3]<-sample_1$class[ which.min(sample_1$Eu_Dist) ] # use which.min, 
+                                                                 # ignores NA
 
 
 # how to implement the process with r fucntionality
+# we would call for knn fucntion from class package
 class::knn( train=sample_1[c(1,2),1], # we define training set, and esclude column with classes 
             test=new_point, # define value ww need to classify
             cl=sample_1[c(1,2),2], # indicate classes which coreslond to known record
@@ -133,8 +131,8 @@ ggplot(data=sample_1,aes(y= feature_BMI ,x=class))+
 
 #######################  PART_2
 
-# Greate jon , now its time to check example with 21 feature , but with 
-#multiple records for each class
+# Horoso , now its time to check example with 1 , but with 
+# multiple records for each class
 sample_3<-data.frame(  feature_BMI=c( runif(3,18.5,24.9),
                                       runif(3, 25.0,29.9)),
                        class=c( rep("Healthy weight",3),rep("Overweight",3) ) )
@@ -146,7 +144,7 @@ ggplot(data=sample_3,aes(y= feature_BMI ,x=class))+
   geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) +
   geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2,color="blue" )
 # Again , now we need to use 6 red poind in total to decide 
-# for which class blue point belongs, therefore, here we estiate
+# for which class blue point belongs, therefore, here we estimate
 # 6 values for Euclidian distacne
 
 #mutate fucntion would be quite handy
@@ -159,8 +157,8 @@ ggplot2::ggplot(data=sample_3,aes(y= feature_BMI ,x=class))+
   geom_point(col="red")+geom_point(aes(y=new_point),col="blue",cex=5) +
   geom_label(aes(label=round(new_point,2),x=class,y=new_point  ),nudge_y = 0.1,nudge_x=0.2,color="blue" )+
   geom_label(aes(label=paste("sqrt","(","(",round(new_point,2),"-",round(feature_BMI[1],2) ,")","^2",")","\n",
-                              "sqrt","(","(",round(new_point,2),"-",round(feature_BMI[2],2) ,")","^2",")","\n",
-                              "sqrt","(","(",round(new_point,2),"-",round(feature_BMI[3],2) ,")","^2",")"),
+                             "sqrt","(","(",round(new_point,2),"-",round(feature_BMI[2],2) ,")","^2",")","\n",
+                             "sqrt","(","(",round(new_point,2),"-",round(feature_BMI[3],2) ,")","^2",")"),
                  x="Healthy weight",y=new_point  ),nudge_y = 2,nudge_x=0 )
 # same calculation are aplied to column Overweignt
 
@@ -171,8 +169,8 @@ sample_3 <-sample_3 %>% dplyr::arrange(Eu_Dist) %>% dplyr::slice(1:3)
 
 table(sample_3$class) # now we use table to count uniqu classes
 prop.table( table(sample_3$class) ) # prop.table to wrup table is used to check share of class
-                                    # among oter classes, we divide count of overweight records by total
-                                    # number of records
+# among oter classes, we divide count of overweight records by total
+# number of records
 
 
 # How should hte process be implemented with R fucntionality
@@ -185,7 +183,7 @@ class::knn( train=sample_3[,1],
 # prop.table , proportion table 
 
 
-        ############ PART 3
+############ PART 3
 
 # Knn is great classifying tool gof data with homogenous 
 # records, for example in iris dataset all features are
@@ -214,7 +212,7 @@ cat(   "sqrt(  (1000-5.0)^2+(2.7-2.0)^2+(3.9-3.5)^2     )"      )
 # class, which correspond to the respecive record,
 # to become meaningfull classifier.
 # the resoective situations may require use or
-# data normalisteion but so far Iris data set in our example
+# data normalisteion ,but so far, Iris data set in our example
 # does not reuqire specia transofrmation
 
 # goind bacj to sample_4
@@ -227,22 +225,22 @@ tibble::glimpse(sample_4)
 
 # lets sample single row we would like to predict
 set.seed(321)
-loc<-sample(1:nrow(sample_4),3 ) # we randomly generate 3 indices
+loc<-sample(1:nrow(sample_4),1 ) # we randomly generate index
 test_row_1<-sample_4[ loc[1]   , -5  ] # here we store 1 record we need to classify
 test_row_cl<-sample_4[ loc[1]   , 5  ] # here we store actual classes for test row
 
 # what classes are available 
 sample_4$Species %>% unique()
-# we need 3 recordsm 1 per each class, therefore
-#our train set woudl contain samples for each class
+# we need 3 records, 1 per each class, therefore
+# our train set woudl contain 1 samples for each class
 
-# we would require to grpup data and take 1 sample from each froup
-train_row_3<-sample_4[-loc,] %>% group_by(Species) %>%
-                    slice(1)
+# we would require to group data and take 1 sample from each froup
+train_row_3<-sample_4[-loc,] %>% dplyr::group_by(Species) %>%
+  slice(1)
 
 # First off all wee ned to draw 4 graphs, 
 # 1 graph per feature where each graph contains
-# 1 point from test set and 2 form training set
+# 1 point from test set and 3 form training set
 
 train_row_3
 test_row_1
@@ -251,15 +249,16 @@ test_row_1
 # we need to decide which is closer in terms of 
 # euclidian distance to test_row_1
 
-# we need to bind data into tibble to produce meaningfull plot.
-# This transformation is srequired 
+# we need to convert train_row_3 and test_row_1 to long format
+# This transformation is requred 
 # to apply ggplot with facets
-gg_train<-train_row_2 %>% tidyr::pivot_longer(Sepal.Length:Petal.Width ,
-                                       names_to="Feature") %>%
-  rename("Class"="Species")  
+gg_train<-train_row_3 %>% tidyr::pivot_longer(Sepal.Length:Petal.Width ,
+                                              names_to="Feature") %>%
+                            rename("Class"="Species")  
+# here we Gather columns data from multiple rows into rows
 
 gg_test<-test_row_1 %>% tidyr::pivot_longer(Sepal.Length:Petal.Width ,
-                                     names_to="Feature") %>%
+                                            names_to="Feature") %>%
   mutate(Class="Unknown") %>% dplyr::select(Class,Feature,value)
 
 gg_in<-dplyr::bind_rows(gg_train,gg_test)
@@ -273,9 +272,10 @@ ggplot2::ggplot(data=gg_in,aes(y=Class,x=value))+
   facet_grid(~Feature,  space="free") +
   theme_bw()
 
-#  red poins is  present per each feature,
+#  Unknown  poins is present per each feature, Corresponding to
+# unknown class
 #  oterh dots are features from 3 instances  
-#  of train set
+#  of train set with defined class
 
 # 3 records and 1 test record means 3 equations, 4 features per record,
 # mean 4 operand in equation
@@ -285,7 +285,7 @@ cat( "sqrt(  (4.8-6)^2+(1.4-2.5)^2+(6.8-6.3)^2+(2.8-3.3)^2    )"      )
 
 #  Here we need to substract train set form test
 train_row_3[,-5]-test_row_1 # both have different rows , we need to triope
-                            # sinfle record in test set
+# sinfle record in test set
 train_row_3[,-5]-test_row_1[ c(1,1,1),]   # here first rows is repeated 3 times
 # this is how to extract values element wise when dealing with 
 # data frames
@@ -293,12 +293,12 @@ train_row_3[,-5]-test_row_1[ c(1,1,1),]   # here first rows is repeated 3 times
 (train_row_3[,-5]-test_row_1[ c(1,1,1),] )^2 # nest spet is to  take square
 
 # the whole eaution with rowSums
-Euc_Dist=rowSums( (train_row_2[,-5]-test_row_1[ c(1,1,1), ])^2 ) %>% sqrt()
+Euc_Dist=rowSums( (train_row_3[,-5]-test_row_1[ c(1,1,1), ])^2 ) %>% sqrt()
 
 # now we eould create additional column and arrange data, then pull 1 nearest
-# neighbours , as number of classes equals number of records
+# neighbours because number of classes equals number of records
 train_row_3<- train_row_3 %>% ungroup() %>% mutate(Euc_Dist=Euc_Dist) %>%
-              arrange(Eu_Dist) %>% slice(1)
+              arrange(Euc_Dist) %>% slice(1)
 test_row_cl
 # we can conclude that 
 
@@ -308,15 +308,16 @@ class::knn( train=train_row_3[,c(-5,-6)],
             prob=TRUE)
 
 
-##### Greate Jon Not we splot shole irus into 80 20 split,
-# 80 for trainig set and 20 for test, therefore we need to classify 20
-# records
+##### Greate Job, Not we would employ whole 
+# iris into 80,20 split,
+# 80% of records for trainig set and 20$ for test, therefore we need to classify 20%
+# of records
 
 set.seed(253)
-loc<-sample(1:nrow(sample_4),20 ) # this are indices for test set
-test_set<-sample_4[ loc   , -5 ]
-test_set_cl<-sample_4[ loc   , 5 ]
-train_set<-sample_4[ -loc   , ]
+loc<-sample(1:nrow(iris),nrow(iris)*0.2 ) # this are indices for test set
+test_set<-iris[ loc   , -5 ]
+test_set_cl<-iris[ loc   , 5 ]
+train_set<-iris[ -loc   , ]
 
 
 # How would knn fucntion perform
@@ -326,16 +327,16 @@ m1_knn<-knn( train=train_set[,-5],
              prob=TRUE)
 
 # now we stimate accuracy, how many values in 
-#m1_knn are equal to values in test set classes
+# m1_knn are equal to values in test set classes
 sum(m1_knn==test_set_cl)/ length(test_set_cl)
 
-##  now lest write our own to soliduty understandng 
+##  now lest write our own solution to soliduty understandng 
 ## of knn 
 
 n_test<-nrow(test_set)
 n_train<-nrow(train_set)
 out<-rep("",n_test) # out vecotr would store our result of classification 
-                    # of train values
+# of test  values
 
 # please dont be discourage by loop, it feets perfecty in 
 # what we want to do
@@ -346,84 +347,29 @@ for ( i in 1:n_test) {
   # lets check each operand
   Eu_Dist=rowSums( (train_set[,-5] - test_set[i,][c(rep(1,n_train)), ] ) ^2 ) %>% sqrt()
   train_buf<-train_set %>% mutate(Eu_Dist=Eu_Dist) %>% arrange(Eu_Dist) %>%
-                       slice(1:3)
+    slice(1:3)
   class_buf<-names( which.max(table(train_buf$Species)))
   out[i]<-class_buf
-         } 
+} 
 
 sum(out==test_set_cl)/ n_test
-
 
 
 ## finaly , lets combine knn fucntion and loop to 
 ## chack if our accuracy can be improved
 
-m1_knn<-knn( train=train_set[,-5],
-             test=test_set,
-             cl=train_set[,5], k=3,
-             prob=TRUE)
-
-# now we stimate accuracy, how many values in 
-#m1_knn are equal to values in test set classes
-sum(m1_knn==test_set_cl)/ length(test_set_cl)
-
 
 # Again, lets proceed with loop
-
+# we woudl ckech accuraccy for k range form 1 to 10
 for (i in (1:10) )  {
   m1_knn<-knn( train=train_set[,-5],
                test=test_set,
                cl=train_set[,5], k=i,
                prob=TRUE)
   acc<-sum(m1_knn==test_set_cl)/ length(test_set_cl)
-     print( paste( i,": Accuracy of ",  acc ) )
+  print( paste( i,": Accuracy of ",  acc ) )
 }
 
-# We can conllude, that accutacy of 1 can be achievedm 
-# meaning that we managed to correcly classify all records
+# We can conllude, that largest accuracy can be achieved at 
 
-
-
-
-
-# # identical 
-# 
-# 
-# # now lets multiply each column by random number
-# loc<-sample(1:nrow(sample_4),20 )
-# sample_5<-iris
-# # sample_5[,c(1:4)]<-apply(sample_5[,c(1:4)],MARGIN=2, 
-# #                        function(x) return( x*runif(1,10,10000))  )
-# # 
-# # sample_5[,c(1:4)]/iris[,c(1:4)] 
-# 
-# test_set<-sample_5[ loc   , -5 ]
-# test_set_cl<-sample_5[ loc   , 5 ]
-# train_set<-sample_5[ -loc   , ]
-# lapply(train_set[,1:4],range)
-# 
-# m1_knn<-knn( train=train_set[,-5],
-#              test=test_set,
-#              cl=train_set[,5], k=3,
-#              prob=TRUE)
-# 
-# sum(m1_knn==test_set_cl)/ n_test
-# 
-# 
-# normalize<-function(x){
-#   return((x-min(x))/(max(x)-min(x)))
-# }      
-# 
-# train_set[,-5]<-apply(train_set[,-5], MARGIN=2 ,normalize)
-# test_set<-apply(test_set, MARGIN=2 ,normalize)
-# lapply(train_set[,1:4],range)
-# 
-# 
-# m1_knn<-knn( train=train_set[,-5],
-#              test=test_set,
-#              cl=train_set[,5], k=3,
-#              prob=TRUE)
-# 
-# sum(m1_knn==test_set_cl)/ n_test
-# 
 
